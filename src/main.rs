09a1e1b100fec508c99841984dd25412a3013dce
Siwat Pruksapanya <siwat.pru@gmail.com>
2024-02-02 22:36:35 +0700

    Add tera template + HTMX button!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_dyn_templates::{context, Template};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context! {})
+}
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello from the server :)!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/api", routes![hello])
+        .attach(Template::fairing())
 }
